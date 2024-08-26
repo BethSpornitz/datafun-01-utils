eba2b0610f0b2ebf6444f9756cb472963de01fd7
Beth Spornitz <bethspornitz@gmail.com>
2024-08-26 18:11:08 -0500

    ITERATION 2

--- utils_spornitz.py
+++ utils_spornitz.py
@@ -1,15 +1,12 @@
-''' ITERATION 1
+''' ITERATION 2
 
 Module: Synapse Analytics - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
 
 Process:
 
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
+Here's what a second draft of my utils_case.py might look like:
 
 1. I start with this docstring at the very beginning.
    I use it to clarify the purpose of my Python file and organize my thoughts.
@@ -24,31 +21,31 @@ Here's what a first draft of my utils_case.py might look like:
 I'll test it in an online interpreter to ensure this version runs correctly before continuing.
 '''
 
-#####################################
+####################################
 # Declare a global variable named byline.
 #####################################
 
-byline: str = 'Synapse Analytics: Deeper Insights. Clearer Decisions'
+byline: str = 'Synapse Analytics: Deeper Insights. Clearer Decisions.'
+
+#####################################
+# Define the get_byline() Function
+#####################################
+
+def get_byline() -> str:
+    '''Return a byline for my analytics projects.'''
+    return byline
 
 #####################################
 # Define a main() function for this module.
 #####################################
 
-# Create a function named main.
-# A function is a block of code that performs a specific task.
-# This function will simply print the byline to the console.
-# Add a type hint to indicate that this function doesn't return anything when called 
-# (that is, it has a Python type of None).
-# It doesn't need any additional information passed in, 
-# so there's nothing needed inside the parentheses.
-# Everything afer the colon (:) must be indented (usually 4 spaces)
-
+# The main function now calls get_byline() to retrieve the byline.
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
-    print(byline)
+    print(get_byline())
 
 #####################################
-# Conditional Execution - Only call main() when executing this module as a script.
+# Conditional Execution
 #####################################
 
 if __name__ == '__main__':
