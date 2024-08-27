3d260a201f42253951e3a1144686c6b3aaae8b56
Beth Spornitz <bethspornitz@gmail.com>
2024-08-26 20:36:29 -0500

    ITERATION 4

--- utils_spornitz.py
+++ utils_spornitz.py
@@ -1,4 +1,4 @@
-''' ITERATION 3
+''' ITERATION 4
 
 Module: Synapse Analytics - Reusable Module for My Data Analytics Projects
 
@@ -6,12 +6,24 @@ This module provides a simple, reusable foundation for my analytics projects.
 
 Process:
 
-In this third iteration, I declare additional variables to show skills with different data types.
+In this fourth iteration, I introduce some basic statistics using Python.
+    - min() is a built in function to find the smallest value passed in
+    - max() is a built in function to find the largest value passed in
+    - The statistics module offers methods to calculate mean and standard deviation.
 '''
+#####################################
+# Import Modules at the Top
+#####################################
+
+# In Python, we can import modules to add extra tools and functions.
+# Below, we're importing:
+# - `statistics`: This gives us tools to calculate things like averages.
+# Use CTRL F and type statistics to see where it is used in the code. 
+
+import statistics
 
 #####################################
-# Declare global variables - keep byline at the end
-# We will use this information in a smarter byline
+# Declare global variables
 #####################################
 
 # Boolean variable to indicate if the company has international clients
@@ -19,6 +31,9 @@ has_international_clients: bool = True
 
 # Integer variable for the number of years in operation
 years_in_operation: int = 10
+
+# Float variable for the average client satisfaction score
+average_client_satisfaction: float = 4.7
 
 # List of strings representing the skills offered by the company
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
@@ -35,11 +50,32 @@ has_female_analysts = True
 # Integer variable for the number of employees
 number_employees: int = 150
 
-# List of integers representing the number of years experience of the top five analysts
+# List of floats representing the number of years experience of the top five analysts
 years_of_experience: list = [30, 28, 22, 18, 15]
+
+# float variable for the average years of experience
+average_years_of_experience: float = 22.6
 
 # List of cities where we serve Clients
 locations:  list = ['Tampa', 'Kansas City', 'New York']
+
+#####################################
+# Calculate Basic Statistics 
+#   Do this BEFORE we declare the byline 
+#   So the values have been calculated 
+#   and are ready for use in the byline.
+#####################################
+
+# Calculate basic stats using built-in functions min(), max() and statistics module functions mean() and stdev().
+min_score: float = min(client_satisfaction_scores)  
+max_score: float = max(client_satisfaction_scores)  
+mean_score: float = statistics.mean(client_satisfaction_scores)  
+stdev_score: float = statistics.stdev(client_satisfaction_scores)
+
+min_years_experience: float = min(years_of_experience)  
+max_years_experience: float = max(years_of_experience)  
+mean_years_experience: float = statistics.mean(years_of_experience)  
+stdev_years_experience: float = statistics.stdev(years_of_experience)
 
 #####################################
 # Declare a global variable named byline. 
@@ -59,7 +95,14 @@ Has Female Analysts:    {has_female_analysts}
 Number of Employees:    {number_employees}
 Locations Served:    {locations}
 Years of Experience of our top five analysts:    {years_of_experience}
-
+Minimum Client Satisfaction Score:    {min_score}
+Maximum Client Satisfaction Score:   {max_score}
+Average Client Satisfaction Score:    {mean_score}
+Standard Deviation of Client Satisfaction Scores:    {stdev_score}
+Minimum Years of Experience:    {min_years_experience} 
+Maximum Years of Experience:    {max_years_experience}
+Average Years of Experience:    {mean_years_experience} 
+Standard Deviation of Years of Experience:    {stdev_years_experience}
 """
 
 #####################################
